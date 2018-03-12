88cba06082a2368e419d2b14282520ba45793d6b
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2018-03-12 09:57:16 +0200

    Fix tests

--- test/privat24_clj/core_test.clj
+++ test/privat24_clj/core_test.clj
@@ -2,7 +2,3 @@
   (:require [clojure.test :refer :all]
             [privat24-clj.core :refer :all])
   (:use  clj-http.fake))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- test/privat24_clj/taxes_test.clj
+++ test/privat24_clj/taxes_test.clj
@@ -1,4 +1,4 @@
 (ns privat24-clj.taxes-test
-  (:require [privat24-clj.taxes :as t]
+  (:require [privat24-clj.taxes :refer :all]
             [clj-time.core :as t]
             [clojure.test :refer :all]))
